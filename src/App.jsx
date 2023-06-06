2c2f7e1373425bdf644ad25bc2078cb526cb3f94
ataher-ali <ataher.dev@gmail.com>
2023-06-07 01:18:06 +0600

    basic nav setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Nav from './Layouts/Header/Nav';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      hi
+      <Nav></Nav>
+      <Outlet></Outlet>
     </div>
   );
 };
---
+++ src/Layouts/Header/NavLinks.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavLinks = () => {
+    return (
+        <>
+    
+      <li><a>Item 1</a></li>
+      <li><a>Item 3</a></li>
+   
+        </>
+    );
+};
+
+export default NavLinks;
